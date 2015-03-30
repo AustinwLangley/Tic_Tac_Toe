5f8c43a655c1eeff43f99cfbd3cfba2cc2194ba9
Austin Langley <austinwlangley@gmail.com>
2015-03-29 21:50:04 -0700

    created game board and started game logic

--- js/TictactoeController.js
+++ js/TictactoeController.js
@@ -4,11 +4,53 @@ angular
 
     function TictactoeController(){
         var self = this;
+        var playerTurn = "one"
 
-        self.tictactoeBoard = [ {space: " "},  {space: " "},  {space: " " },
-						        {space: " " }, {space: " " }, {space: " " },
-						        {space: " " }, {space: " " }, {space: " " }
-      
-    ];
-      
-}
+        self.tictactoeBoard = [  {image: "" , hasPlayer: false},  
+	      						 {image: "" , hasPlayer: false},
+	      						 {image: "" , hasPlayer: false},
+	      					     {image: "" , hasPlayer: false},
+	      						 {image: "" , hasPlayer: false},
+	      						 {image: "" , hasPlayer: false},
+	      						 {image: "" , hasPlayer: false},
+	      					     {image: "" , hasPlayer: false},
+	      					     {image: "" , hasPlayer: false}
+      	];
+
+      	self.gameInit = gameInit
+      	self.playerOneTurn = playerOneTurn;
+      	self.playerTwoTurn = playerTwoTurn;
+      	self.playerMove = playerMove
+	
+    //Starts the game  	
+	gameInit();
+
+	function gameInit() {
+	
+		function playerMove(playerTurn){
+			if (playerTurn === "one"){
+				playerOneTurn();
+			}
+
+			else {
+				playerTwoTurn();
+			}
+		}
+
+		function playerOneTurn(){
+			if (hasPlayer === false){
+				self.tictactoeBoard[].image= "./TicTacToe_Images/x.png"  
+				playerTurn ="two";
+			}
+		};
+
+		function playerTwoTurn(){
+			if (hasPlayer === false){
+				self.tictactoeBoard[].image= "./TicTacToe_Images/o.png" 
+				playerTurn = "one"
+			}			
+		}; 
+
+	} //end of function gameInit
+
+} //close TictactoeController	
