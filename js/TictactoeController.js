a7489d14be03b1511a76e4f09209a532c3d8db07
Austin Langley <austinwlangley@gmail.com>
2015-03-30 21:11:10 -0700

    Tic tac toe now works and declares a winner in the console

--- js/TictactoeController.js
+++ js/TictactoeController.js
@@ -5,16 +5,17 @@ angular
     function TictactoeController(){
         var self = this;
         var playerTurn = "one";
+        var winner;
 
-        self.tictactoeBoard = [  {image: "" , hasPlayer: false},  
-	      						 {image: "" , hasPlayer: false},
-	      						 {image: "" , hasPlayer: false},
-	      					     {image: "" , hasPlayer: false},
-	      						 {image: "" , hasPlayer: false},
-	      						 {image: "" , hasPlayer: false},
-	      						 {image: "" , hasPlayer: false},
-	      					     {image: "" , hasPlayer: false},
-	      					     {image: "" , hasPlayer: false}
+        self.tictactoeBoard = [  {image: "", hasPlayer: false, playerOne: false, playerTwo: false},  
+	      						 {image: "", hasPlayer: false, playerOne: false, playerTwo: false},
+	      						 {image: "", hasPlayer: false, playerOne: false, playerTwo: false},
+	      					     {image: "", hasPlayer: false, playerOne: false, playerTwo: false},
+	      						 {image: "", hasPlayer: false, playerOne: false, playerTwo: false},
+	      						 {image: "", hasPlayer: false, playerOne: false, playerTwo: false},
+	      						 {image: "", hasPlayer: false, playerOne: false, playerTwo: false},
+	      					     {image: "", hasPlayer: false, playerOne: false, playerTwo: false},
+	      					     {image: "", hasPlayer: false, playerOne: false, playerTwo: false}
       	];
 
       	self.playerOneTurn = playerOneTurn;
@@ -35,7 +36,10 @@ angular
 				if (self.tictactoeBoard[$index].hasPlayer === false){
 					self.tictactoeBoard[$index].image = "./Tic_Tac_Toe_Images/x.png"  
 					self.tictactoeBoard[$index].hasPlayer = true;
+					self.tictactoeBoard[$index].playerOne = true;
 					playerTurn = "two";
+					winnerCheck();
+
 					return playerTurn;
 				}
 			};
@@ -43,9 +47,111 @@ angular
 				if (self.tictactoeBoard[$index].hasPlayer === false){
 					self.tictactoeBoard[$index].image= "./Tic_Tac_Toe_Images/o.png" 
 					self.tictactoeBoard[$index].hasPlayer = true;
+					self.tictactoeBoard[$index].playerTwo = true;
 					playerTurn = "one"
+					winnerCheck();
 					return playerTurn;
 				}			
 			}; 
 
+			function winnerCheck(){
+				if (self.tictactoeBoard[0].playerOne === true && self.tictactoeBoard[1].playerOne === true && self.tictactoeBoard[2].playerOne === true){
+					winner = "one";
+					console.log(winner);
+				}
+				if 
+				   (self.tictactoeBoard[3].playerOne === true && self.tictactoeBoard[4].playerOne === true && self.tictactoeBoard[5].playerOne === true){
+					winner = "one";
+					console.log(winner);
+				}
+				if 	
+					(self.tictactoeBoard[6].playerOne === true && self.tictactoeBoard[7].playerOne === true && self.tictactoeBoard[8].playerOne === true){
+					winner = "one";
+					console.log(winner);
+				}
+				if 	
+					(self.tictactoeBoard[0].playerOne === true && self.tictactoeBoard[3].playerOne === true && self.tictactoeBoard[6].playerOne === true){
+					winner = "one";
+					console.log(winner);
+				}
+				if 	
+					(self.tictactoeBoard[1].playerOne === true && self.tictactoeBoard[4].playerOne === true && self.tictactoeBoard[7].playerOne === true){
+					winner = "one";
+					console.log(winner);
+				}
+				if 	
+					(self.tictactoeBoard[2].playerOne === true && self.tictactoeBoard[5].playerOne === true && self.tictactoeBoard[8].playerOne === true){
+					winner = "one";
+					console.log(winner);
+				}
+				if 	
+					(self.tictactoeBoard[0].playerOne === true && self.tictactoeBoard[4].playerOne === true && self.tictactoeBoard[8].playerOne === true){
+					winner = "one";
+					console.log(winner);
+				}
+				if 	
+					(self.tictactoeBoard[6].playerOne === true && self.tictactoeBoard[4].playerOne === true && self.tictactoeBoard[2].playerOne === true){
+					winner = "one";
+					console.log(winner);
+				}
+
+
+				if (self.tictactoeBoard[0].playerTwo === true && self.tictactoeBoard[1].playerTwo === true && self.tictactoeBoard[2].playerTwo === true){
+					winner = "two";
+					console.log(winner);
+				}
+				if 
+				   (self.tictactoeBoard[3].playerTwo === true && self.tictactoeBoard[4].playerTwo === true && self.tictactoeBoard[5].playerTwo === true){
+					winner = "two";
+					console.log(winner);
+				}
+				if 	
+					(self.tictactoeBoard[6].playerTwo === true && self.tictactoeBoard[7].playerTwo === true && self.tictactoeBoard[8].playerTwo === true){
+					winner = "two";
+					console.log(winner);
+				}
+				if 	
+					(self.tictactoeBoard[0].playerTwo === true && self.tictactoeBoard[3].playerTwo === true && self.tictactoeBoard[6].playerTwo === true){
+					winner = "two";
+					console.log(winner);
+				}
+				if 	
+					(self.tictactoeBoard[1].playerTwo === true && self.tictactoeBoard[4].playerTwo === true && self.tictactoeBoard[7].playerTwo === true){
+					winner = "two";
+					console.log(winner);
+				}
+				if 	
+					(self.tictactoeBoard[2].playerTwo === true && self.tictactoeBoard[5].playerTwo === true && self.tictactoeBoard[8].playerTwo === true){
+					winner = "two";
+					console.log(winner);
+				}
+				if 	
+					(self.tictactoeBoard[0].playerTwo === true && self.tictactoeBoard[4].playerTwo === true && self.tictactoeBoard[8].playerTwo === true){
+					winner = "two";
+					console.log(winner);
+				}
+				if 	
+					(self.tictactoeBoard[6].playerTwo === true && self.tictactoeBoard[4].playerTwo === true && self.tictactoeBoard[2].playerTwo === true){
+					winner = "two";
+					console.log(winner);
+				}
+
+				if ((self.tictactoeBoard[0].hasPlayer === true) &&
+					(self.tictactoeBoard[1].hasPlayer === true) &&
+					(self.tictactoeBoard[2].hasPlayer === true) &&
+					(self.tictactoeBoard[3].hasPlayer === true) &&
+					(self.tictactoeBoard[4].hasPlayer === true) &&
+					(self.tictactoeBoard[5].hasPlayer === true) &&
+					(self.tictactoeBoard[6].hasPlayer === true) && 
+					(self.tictactoeBoard[7].hasPlayer === true) &&
+					(self.tictactoeBoard[8].hasPlayer === true) &&
+					(winner !== "one") &&
+					(winner !== "two")){
+					winner = "cat";
+					console.log(winner);
+				}
+			} //close winner function
+
 }; //close TictactoeController	
+
+
